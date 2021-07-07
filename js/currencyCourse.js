b05454e107e6a49cd25445fd9088cae663c02a50
liubart lojka <liubart@tut.by>
2021-07-07 13:56:27 +0300

    fixed bugs...

--- js/currencyCourse.js
+++ js/currencyCourse.js
@@ -1,12 +1,13 @@
-function getCurrencyCourse(){
+function getCurrencyCourse() {
     return fetch('https://www.nbrb.by/api/exrates/rates?periodicity=0')
-    .then(data=>data.json());
+        .then(data => data.json());
 }
 
-function fillTableOfCourses(table, courses){
-    for(let course of courses){
+function fillTableOfCourses(table, courses) {
+    for (let course of courses) {
         let row = table.insertRow(-1);
-        row.insertCell(-1).innerHTML=course.Cur_Name;
-        row.insertCell(-1).innerHTML=(course.Cur_OfficialRate/course.Cur_Scale).toFixed(4);
+        row.insertCell(-1).innerHTML = course.Cur_Name;
+        row.insertCell(-1).innerHTML = course.Cur_Scale + ' ' + course.Cur_Abbreviation;
+        row.insertCell(-1).innerHTML = (course.Cur_OfficialRate).toFixed(4);
     }
 }
